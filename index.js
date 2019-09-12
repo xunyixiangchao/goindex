4d59dd767277e79736cc1059e7675aa46bc1fbad
donwa <hi0660@gmail.com>
2019-09-12 20:04:27 +0800

    theme select

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var authConfig = {
     "siteName": "GoIndex", // 网站名称
     "root_pass": "index",  // 根目录密码，优先于.password
     "version" : "1.0.6", // 程序版本
-    "theme" : "material",
+    "theme" : "material", // material  classic 
     "client_id": "202264815644.apps.googleusercontent.com",
     "client_secret": "X4Z3ca8xfWDb1Voo-F9a7ZxJ",
     "refresh_token": "", // 授权 token
